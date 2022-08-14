eff9d3ed631e29e1b8dc376b3eeca58c49413653
Mr.Farzaneh <amintavakkoli.fi@gmail.com>
2022-08-14 13:01:14 +0430

    add income and patientList field in doctor class.

--- src/main/java/org/example/entity/Doctor.java
+++ src/main/java/org/example/entity/Doctor.java
@@ -3,18 +3,26 @@ package org.example.entity;
 import org.example.base.entity.BaseEntity;
 import org.example.base.entity.Person;
 
+import java.util.List;
+
 public class Doctor extends Person {
     private String username;
     private String password;
     private MedicalField medicalField;
 
+    private List<Patient> patientList;
+
+    private double income;
+
     public Doctor() {
     }
 
-    public Doctor(String username, String password, MedicalField medicalField) {
+    public Doctor(String username, String password, MedicalField medicalField, List<Patient> patientList, double income) {
         this.username = username;
         this.password = password;
         this.medicalField = medicalField;
+        this.patientList = patientList;
+        this.income = income;
     }
 
     public String getUsername() {
@@ -41,12 +49,30 @@ public class Doctor extends Person {
         this.medicalField = medicalField;
     }
 
+    public List<Patient> getPatientList() {
+        return patientList;
+    }
+
+    public void setPatientList(List<Patient> patientList) {
+        this.patientList = patientList;
+    }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+
     @Override
     public String toString() {
         return "Doctor{" +
                 "username='" + username + '\'' +
                 ", password='" + password + '\'' +
                 ", medicalField=" + medicalField +
+                ", patientList=" + patientList +
+                ", income=" + income +
                 '}';
     }
 }
